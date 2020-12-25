67f623cf315a975f23af4d6dc500119535c05c9a
diegodorrejo123 <50970799+diegodorrejo123@users.noreply.github.com>
2020-12-25 00:05:31 -0400

    asdas

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -60,7 +60,7 @@ class CategoryController extends Controller
                 $res = [
                     'code' => 400,
                     'status' => 'error',
-                    'message' => 'Ocurrió un error en los datos de la categoría',
+                    'message' => 'Formulario inválido',
                     'errors' => $validate->errors()
                 ];
             }else{
@@ -78,6 +78,56 @@ class CategoryController extends Controller
                 'status' => 'error',
                 'message' => 'No hemos recibido datos de la categoría'
             ];
+        }
+        return response()->json($res, $res['code']);
+    }
+    public function update($id, Request $request)
+    {
+        $data = json_decode($request->input('json', null), true);
+
+        if(is_null($data)){
+            $res = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No hemos recibido datos de la categoría'
+            ];
+        }else{
+            $validate = Validator::make($data, [
+                'name' => 'required'
+            ]);
+            if($validate->fails()){
+                $res = [
+                    'code' => 400,
+                    'status' => 'error',
+                    'message' => 'Formulario inválido',
+                    'errors' => $validate->errors()
+                ];
+            }else{
+                $category = Category::find($id);
+                if(is_object($category)){
+                    $update = $category->update($data);
+                    if($update){
+                        $res = [
+                            'code' => 200,
+                            'status' => 'success',
+                            'message' => 'Se han guardado los cambios',
+                            'category' => $data
+                        ];
+                    }else{
+                        $res = [
+                            'code' => 400,
+                            'status' => 'error',
+                            'message' => 'Ha ocurrido un error al actualizar los cambios',
+                        ];
+                    }
+                }else{
+                    $res = [
+                        'code' => 400,
+                        'status' => 'error',
+                        'message' => 'No existe la categoría',
+                    ];
+                }
+            }
         }
         return response()->json($res, $res['code']);
     }
